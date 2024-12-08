0850f655ae837f8dfcc7fed07ea184220cae2333
Arnold1506 <aniketm1506@gmail.com>
2024-12-08 21:59:40 +0530

    delete invoice changes

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -113,10 +113,10 @@ export async function deleteInvoice(id: string) {
   try {
     await sql`DELETE FROM invoices WHERE id = ${id}`;
     revalidatePath('/dashboard/invoices');
-    return { message: 'Deleted Invoice.' };
+    // return { message: 'Deleted Invoice.' };
   } catch (error) {
     console.log(error);
-    return { message: 'Database Error: Failed to Delete Invoice.' };
+    // return { message: 'Database Error: Failed to Delete Invoice.' };
   }
 }
 
--- app/ui/invoices/buttons.tsx
+++ app/ui/invoices/buttons.tsx
@@ -26,9 +26,12 @@ export function UpdateInvoice({ id }: { id: string }) {
 }
 
 export function DeleteInvoice({ id }: { id: string }) {
-  const deleteInvoiceWithId = deleteInvoice.bind(null, id);
+  const deleteInvoiceWithId = async (event: React.FormEvent) => {
+    event.preventDefault();
+    await deleteInvoice(id);
+  };
   return (
-    <form action={deleteInvoiceWithId}>
+    <form onSubmit={deleteInvoiceWithId}>
       <button className="rounded-md border p-2 hover:bg-gray-100">
         <span className="sr-only">Delete</span>
         <TrashIcon className="w-5" />
